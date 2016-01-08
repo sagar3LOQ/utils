944e1f450698c91b1a56f065aabf3ff7175adc59
Sagar Sahu <sagar.sahu@3loq.com>
2016-01-08 12:35:38 +0530

    added in out directory and auto label generation functionality

--- src/convert2text.py
+++ src/convert2text.py
@@ -16,7 +16,7 @@ import docx2txt
 
 class CVParser(object):
     # Some commonly used regex patterns:
-    def __init__(self, cvfile,label, password=None):
+    def __init__(self, cvfile,label,outDir, password=None):
         self.errorMsg = None
         self.cvFile = cvfile # Input CV file - can be in any of the following formats: pdf, doc, docx, rtf, txt, odt.
         if not os.path.exists(self.cvFile) or not os.path.isfile(self.cvFile):
@@ -24,13 +24,13 @@ class CVParser(object):
             return None
         self.cvFormat = None # Can be any of the following: pdf, doc, docx, rtf, txt, odt.
         self.cvFilePasswd = password # password for the input CV file (if one exists).
-        self.scratchDir =  os.path.dirname(self.cvFile) + os.path.sep + "temp_files" # Directory where the intermediate output files will be created (like, while parsing a pdf file, we need to convert it to a text file first, and then parse the text file. This is the location where the text file will be created.
-        if not os.path.exists(self.scratchDir):
-            os.makedirs(self.scratchDir)
+        self.outDir =  outDir
+        if not os.path.exists(self.outDir):
+            os.makedirs(self.outDir)
         self.cvTextFile = None
-        randomStr = 1
+ #       randomStr = 1
         self.label = label
-        self.userTempDir = self.scratchDir + os.path.sep + "tmp_" + randomStr.__str__()
+#        self.userTempDir = self.outDir + os.path.sep + "tmp_" + randomStr.__str__()
 
 
     """
@@ -43,17 +43,14 @@ class CVParser(object):
         ext = fileparts.pop()
         ext = ext.lower()
         primaryEnc, secondaryEnc = filetype.split("/")
-     #   print primaryEnc
-    #    print secondaryEnc
+
         if ext == 'pdf' or ext == 'doc' or ext == 'docx' or ext =='rtf':
             self.cvFormat = ext
- #           print ext
- #           print "test pass format"
+
             return(True)
         elif ext == 'text' :
             self.cvFormat = 'txt'
-  #          print ext
-   #         print "raw text"
+
             return(True)
             
         else:
@@ -86,8 +83,9 @@ class CVParser(object):
     def _convert_pdf_to_text(self,index):
       #  print "processing pdfs"
         input_pdf = self.cvFile
-       # print self.cvFile
-        outputPath = self.scratchDir
+
+
+        outputPath = self.outDir
         inputPath = os.getcwd()
         if os.path.exists(input_pdf):
             inputPath = os.path.dirname(input_pdf)
@@ -97,22 +95,22 @@ class CVParser(object):
         randomStr = int(time.time())
         output_filename = outputPath + os.path.sep + "".join(str(index)) + "_"+str(self.label)   + r".txt"
         self.cvTextFile = output_filename
-     #   print "writing output to {0}".format(output_filename)
+
         output_filename = output_filename.replace (" ", "_")
         outfp = open(output_filename, 'w')
 
         text = textract.process(input_pdf)
  
         outfp.write(text)
-    #    print "written sucessfully"
+
         outfp.close()
         return (0)
 
     def _convert_rtf_to_text(self,index):
    #     print "processing rtf"
         input_pdf = self.cvFile
-  #      print self.cvFile
-        outputPath = self.scratchDir
+
+        outputPath = self.outDir
         inputPath = os.getcwd()
         if os.path.exists(input_pdf):
             inputPath = os.path.dirname(input_pdf)
@@ -122,34 +120,29 @@ class CVParser(object):
         randomStr = int(time.time())
         output_filename = outputPath + os.path.sep + "".join(str(index)) + "_"+str(self.label)   + r".txt"
         self.cvTextFile = output_filename
-  #      print "writing output to {0}".format(output_filename)
+
         outfp = open(output_filename, 'w')
         text = textract.process(input_pdf)
         outfp.write(text)
- #       print "written sucessfully"
+
         outfp.close()
         return (0)
     
     def _convert_doc_to_text(self,index, password=None):
-  #      print "decoding doc file"
+
         input_doc = self.cvFile
-        outputPath = self.scratchDir
-  #      print outputPath
+        outputPath = self.outDir
+
         inputPath = os.getcwd()
         if os.path.exists(input_doc):
             inputPath = os.path.dirname(input_doc)
- #       print inputPath
+
         input_filename = os.path.basename(input_doc)
         input_parts = input_filename.split(".")
-#        print input_parts
- #       input_parts.pop()
-#        print 'hello'
- #       print ".".join(str(index))
+        print "testing"
         output_filename = outputPath + os.path.sep + "".join(str(index)) + "_"+str(self.label)  + r".txt"        #output_filename = output_filename.replace (" ", "_")
- #       print output_filename
- #       print "writing output to {0}".format(output_filename)
+
         self.cvTextFile = output_filename
-#        print "written sucessfully"
         cmd = 'catdoc "%s" > "%s"'%(self.cvFile, self.cvTextFile) # Dangerous!!! Why not use 'subprocess'?
         os.system(cmd)
         return(0)
@@ -157,9 +150,10 @@ class CVParser(object):
 
 
     def _convert_docx_to_text(self,index, password=None):
+
 #        print "Decoding docx file"
         input_docx = self.cvFile
-        outputPath = self.scratchDir
+        outputPath = self.outDir
         inputPath = os.getcwd()
         if os.path.exists(input_docx):
             inputPath = os.path.dirname(input_docx)
@@ -168,25 +162,27 @@ class CVParser(object):
         input_parts.pop()
         randomStr = int(time.time())
         output_filename = outputPath + os.path.sep + "".join(str(index)) + "_"+str(self.label)   + r".txt"
- #       print "writing output to {0}".format(output_filename)
 
+ #       print "writing output to {0}".format(output_filename)
         text = docx2txt.process(input_docx)
         fw = open(output_filename, "w")
- #       print "test"
+
         fw.write(text.encode('utf-8'))
- #       print "written sucessfully"
         fw.close()
         return(0)
 
 
-def convertFiles(dir_name,label):
+def convertFiles(in_dir,label,out_dir):
     index = 0
-    for root, dirs, files in os.walk(dir_name):
+    for root, dirs, files in os.walk(in_dir):
+#    for cvfile in os.listdir(in_dir):
         for file in files:
             index += 1
             cvfile = os.path.join(root, file)
-            print cvfile
-            cvparser = CVParser(cvfile,label)
+            print "Processing :: " + cvfile
+            in_fname = genLabel(cvfile)
+            out_fname = ""
+            cvparser = CVParser(cvfile,label,out_dir)
             if cvparser.errorMsg:
                 print cvparser.errorMsg
                 sys.exit(0)
@@ -196,24 +192,27 @@ def convertFiles(dir_name,label):
                 print "Conversion Failed :("
                 sys.exit(1)
 
-def genLabel(in_path):
-    return in_path.split("/")[-1]
+def genLabel(strg):
+    return strg.split("/")[-1]
 	
 
 if __name__ == '__main__':
 #    cvfile = sys.argv[1]
 
-    accept_dir = ""
+    accept_dir = "/home/viswanath/workspace/code_garage/conver2txt/raw_data/accept"
+    accept_out = "/home/viswanath/workspace/code_garage/conver2txt/raw_text/accept"
     label = genLabel(accept_dir)
-    convertFiles(accept_dir,label)
+    convertFiles(accept_dir,label,accept_out)
 
-    reject_dir = ""
+    reject_dir = "/home/viswanath/workspace/code_garage/conver2txt/raw_data/reject"
+    reject_out = "/home/viswanath/workspace/code_garage/conver2txt/raw_text/reject"
     label = genLabel(reject_dir)
-    convertFiles(reject_dir,label)
+    convertFiles(reject_dir,label,reject_out)
 
 
-    predict_dir = ""
+    predict_dir = "/home/viswanath/workspace/code_garage/conver2txt/raw_data/predict"
+    predict_out = "/home/viswanath/workspace/code_garage/conver2txt/raw_text/predict"
     label = genLabel(predict_dir)
-    convertFiles(predict_dir,label)
+    convertFiles(predict_dir,label,predict_out)
 
 
