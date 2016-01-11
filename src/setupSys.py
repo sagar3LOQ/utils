b2d8f9d5c4a8a3e66fe464ac8030f89728bc808d
Sagar Sahu <sagar.sahu@3loq.com>
2016-01-11 19:30:29 +0530

    main function for controlling app

---
+++ src/setupSys.py
@@ -0,0 +1,20 @@
+import shutil
+
+def moveData(src,dest):
+	shutil.copy(src,dest)
+
+def shuffleData(dirN,trDir,tstDir,split=30):
+
+
+if __name__ == '__main__':
+
+	accept_dir = "/home/viswanath/workspace/code_garage/conver2txt/raw_text/accept"	
+	reject_dir = "/home/viswanath/workspace/code_garage/conver2txt/raw_text/reject"
+	predict_dir = "/home/viswanath/workspace/code_garage/conver2txt/raw_text/predict"
+
+	input_train = "/home/viswanath/workspace/code_garage/conver2txt/in_data/train"
+	input_test = "/home/viswanath/workspace/code_garage/conver2txt/in_data/test"
+	input_predict = "/home/viswanath/workspace/code_garage/conver2txt/in_data/predict"
+	input_total = "/home/viswanath/workspace/code_garage/conver2txt/in_data/total"
+	
+
