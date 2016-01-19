73d133f465861e53c2081a1664388343e1e0b785
Sagar Sahu <sagar.sahu@3loq.com>
2016-01-19 17:11:12 +0530

    added timer object and clean code as per OOPs Design

--- src/testPostgres.py
+++ src/testPostgres.py
@@ -1,15 +1,20 @@
 import psycopg2
 from convert2text_IterText import convertFiles2TextIterWrap
+from timeMeasure import timeMeasure
 
 class PostgreClient:
 
     def __init__(self):
+        self.conn = None
+        self.cur = None
 
     def connectPostgre(self):
-        return psycopg2.connect(database="testdb", user="postgres", password="sin2win", host="127.0.0.1", port="5432")
+        self.conn = psycopg2.connect(database="testdb", user="postgres", password="sin2win", host="127.0.0.1", port="5432")
+        return self.conn
 
-    def getDBPointer(self,conn):
-        return  conn.cursor()
+    def getDBPointer(self):
+        self.cur = self.conn.cursor()
+        return  self.cur
 
 
     def insertData(self,dataGen):
@@ -21,16 +26,19 @@ class PostgreClient:
                      SELECT '"+ meta[0] + "', '" + data[0] +"', '" + meta[1] + "', '" + data[2] + "' \
                      WHERE NOT EXISTS (SELECT 1 FROM RESUME WHERE md5= '" + meta[0] + "')"
         
-            cur.execute(query);
+            self.cur.execute(query);
 
 
     def deleteData(self):
+        return
 
     def updateData(self):
+        return
 
-    def getAllData(self,cur):
-        cur.execute("SELECT md5, fname, label, text1  from RESUME")
-        return cur.fetchall()
+
+    def getAllData(self):
+        self.cur.execute("SELECT md5, fname, label, text1  from RESUME")
+        return self.cur.fetchall()
 
     def printData(self,rows):
         for row in rows:
@@ -42,47 +50,53 @@ class PostgreClient:
 
 
 if __name__ == '__main__':
+
 # Connection to Postgresql DB
+
     try:
 
-        # Connect to the testdb database on our postgres  running at 127.0.0.1:5432
-    #    conn = psycopg2.connect(database="testdb", user="postgres", password="sin2win", host="127.0.0.1", port="5432")
 
+        tm = timeMeasure()
+
+        tm.start()
+        PgCL = PostgreClient()
+        PgCL.connectPostgre()
+        t1 = tm.stop()
         print "Opened database successfully"     #   print "Connected successfully!!!"
+
     except :
+
         print "Could not connect to Postgre" 
 
 
-#    cur = conn.cursor()
+    PgCL.getDBPointer()
+
     print "Started code"
-    accept_dir = "/home/viswanath/workspace/code_garage/conver2txt/raw_data/accept"
+#    accept_dir = "/home/viswanath/workspace/code_garage/conver2txt/raw_data/accept"
+    accept_dir = "/home/viswanath/Downloads/total_resume"
     accept_out = "/home/viswanath/workspace/code_garage/conver2txt/raw_text/accept"
-#    convertDirFiles(accept_dir,accept_out)
+
+
     dataGen = convertFiles2TextIterWrap(accept_dir)
-#    for data in dataGen:
-
- #       meta = data[1].split("_")
-#
-  #      query = "INSERT INTO RESUME (md5, fname, label, text1) \
-    #                 SELECT '"+ meta[0] + "', '" + data[0] +"', '" + meta[1] + "', '" + data[2] + "' \
-   #                  WHERE NOT EXISTS (SELECT 1 FROM RESUME WHERE md5= '" + meta[0] + "')"
-        
-     #   cur.execute(query);
-        
-        conn.commit()
-        print "Records created successfully";
 
 
- #       cur.execute("SELECT md5, fname, label, text1  from RESUME")
-  #      rows = cur.fetchall()
-  #      for row in rows:
-  #          print "MD5 = ", row[0]
-   #         print "FNAME = ", row[1]
-   #         print "LABEL = ", row[2]
-    #        print "TEXT = ", row[3], "\n\n"
+    tm.start()
+    PgCL.insertData(dataGen)
 
-     #   print "Operation done successfully";
 
-    conn.close()
-    
+    PgCL.conn.commit()
+    t2 = tm.stop()
+
+    tm.start()
+    PgCL.getAllData()
+    t3 = tm.stop()
+
+#    PgCL.printData(PgCL.getAllData())
+
+
+    PgCL.conn.close()
+    print "time to connect:: " + str(t1) + "sec"
+    print "time to insert:: " + str(t2) + "sec"
+    print "time to fetchAll:: " + str(t3) + "sec"
+
 
