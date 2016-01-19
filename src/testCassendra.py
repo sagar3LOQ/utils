232f4342c7366df9ec56bd2731db002dc6e164e6
Sagar Sahu <sagar.sahu@3loq.com>
2016-01-19 17:10:56 +0530

    added timer object and clean code as per OOPs Design

--- src/testCassendra.py
+++ src/testCassendra.py
@@ -4,19 +4,29 @@ from cqlengine import connection
 from cqlengine.management import sync_table
 from convert2text_IterText import convertFiles2TextIterWrap
 from cqlengine.exceptions import LWTException
+from timeMeasure import timeMeasure
+
+# Define a model
+class RUser(Model):
+  fname = columns.Text()
+  md5 = columns.Text(primary_key=True)
+  label = columns.Text()
+  text = columns.Text()
+ 
+
+
+
 
 class CassandraClient:
 
     def __init__(self):
-        return
+        self.err= None
 
 
     def connectCassandra(self):
-        connection.setup(['127.0.0.1'], "resumeKS")
-        sself.syncModel()
-
-    def syncModel(self):
+        connection.setup(['127.0.0.1'], "resumeks")
         sync_table(RUser)
+
 
     def insertData(self, dataGen):
         for data in dataGen:
@@ -32,8 +42,10 @@ class CassandraClient:
 
 
     def deleteData(self):
+        return
 
     def updateData(self):
+        return
 
     def getAllData(self):
         return RUser.objects()
@@ -45,26 +57,29 @@ class CassandraClient:
 
 
 
-# Define a model
-class RUser(Model):
-  fname = columns.Text()
-  md5 = columns.Text(primary_key=True)
-  label = columns.Text()
-  text = columns.Text()
- 
-  def __repr__(self):
-    return '%s %s' % (self.fname, self.md5)
 
 
 if __name__ == '__main__':
+
+
+    CasCl = CassandraClient()
+
+
+    tm = timeMeasure()
+
+
 # Connection to Cassendra DB
     try:
 
+  
+        tm.start()
+
         # Connect to the resumeKS keyspace on our cluster running at 127.0.0.1
-    #    connection.setup(['127.0.0.1'], "resumeKS")
+        connection.setup(['127.0.0.1'], "resumeKS")
 
         # Sync your model with your cql table
-    #    sync_table(RUser)
+   #     sync_table(RUser)
+        t1 = tm.stop()
         print "Connected successfully!!!"
     except :
         print "Could not connect to Cassandra" 
@@ -72,23 +87,28 @@ if __name__ == '__main__':
 
 
     print "Started code"
-    accept_dir = "/home/viswanath/workspace/code_garage/conver2txt/raw_data/accept"
+#    accept_dir = "/home/viswanath/workspace/code_garage/conver2txt/raw_data/accept"
+    accept_dir = "/home/viswanath/Downloads/total_resume"
     accept_out = "/home/viswanath/workspace/code_garage/conver2txt/raw_text/accept"
-#    convertDirFiles(accept_dir,accept_out)
+
+
+
+
     dataGen = convertFiles2TextIterWrap(accept_dir)
-#    for data in dataGen:
 
- #       meta = data[1].split("_")
 
-  #      try:
+    tm.start()
+    CasCl.insertData(dataGen)
 
-    #        RUser.if_not_exists().create(fname = data[0],md5 =meta[0], label =meta[1], text = data[2])
-   #     except  LWTException as e:
+    t2 = tm.stop()
 
-     #       print e # existing object
 
-#    cur = RUser.objects()
+    tm.start()
     
- #   for d in cur:
-  #      print d
-#        print "\n\n"
+    CasCl.getAllData()
+    t3 = tm.stop()
+
+
+    print "time to connect:: " + str(t1) + "sec"
+    print "time to insert:: " + str(t2) + "sec"
+    print "time to fetchAll:: " + str(t3) + "sec"
